ec2a462d3d182fe9c805637fcadc6f81bc175796
ctinsman <ctinsman@thegraysonschool.org>
2025-02-19 10:02:15 -0500

    Comment out references to unused motors

--- src/main.py
+++ src/main.py
@@ -20,9 +20,9 @@ controller = Controller()
 
 # Drive motors
 left_drive_1 = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
-left_drive_2 = Motor(Ports.PORT11, GearSetting.RATIO_18_1, False)
+# left_drive_2 = Motor(Ports.PORT11, GearSetting.RATIO_18_1, False)
 right_drive_1 = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
-right_drive_2 = Motor(Ports.PORT20, GearSetting.RATIO_18_1, True)
+# right_drive_2 = Motor(Ports.PORT20, GearSetting.RATIO_18_1, True)
 
 # Arm and claw motors will have brake mode set to hold
 # Claw motor will have max torque limited
@@ -30,8 +30,8 @@ claw_motor = Motor(Ports.PORT3, GearSetting.RATIO_18_1, False)
 arm_motor = Motor(Ports.PORT8, GearSetting.RATIO_18_1, False)
 
 # Auxilary motors
-motor_aux_1 = Motor(Ports.PORT5, GearSetting.RATIO_18_1, False)
-motor_aux_2 = Motor(Ports.PORT7, GearSetting.RATIO_18_1, False)
+# motor_aux_1 = Motor(Ports.PORT5, GearSetting.RATIO_18_1, False)
+# motor_aux_2 = Motor(Ports.PORT7, GearSetting.RATIO_18_1, False)
 
 # Max motor speed (percent) for motors controlled by buttons
 MAX_SPEED = 40
@@ -57,8 +57,8 @@ def drive_task():
         #
         control_l1  = (controller.buttonL1.pressing() - controller.buttonL2.pressing()) * MAX_SPEED
         control_r1  = (controller.buttonR1.pressing() - controller.buttonR2.pressing()) * MAX_SPEED
-        control_l2  = (controller.buttonUp.pressing() - controller.buttonDown.pressing()) * MAX_SPEED
-        control_r2  = (controller.buttonA.pressing() - controller.buttonB.pressing()) * MAX_SPEED
+        # control_l2  = (controller.buttonUp.pressing() - controller.buttonDown.pressing()) * MAX_SPEED
+        # control_r2  = (controller.buttonA.pressing() - controller.buttonB.pressing()) * MAX_SPEED
 
         # joystick tank control
         drive_left = controller.axis3.position()
@@ -76,17 +76,17 @@ def drive_task():
 
         # The drivetrain
         left_drive_1.spin(FORWARD, drive_left, PERCENT)
-        left_drive_2.spin(FORWARD, drive_left, PERCENT)
+        # left_drive_2.spin(FORWARD, drive_left, PERCENT)
         right_drive_1.spin(FORWARD, drive_right, PERCENT)
-        right_drive_2.spin(FORWARD, drive_right, PERCENT)
+        # right_drive_2.spin(FORWARD, drive_right, PERCENT)
 
         # Claw and Arm motors
         arm_motor.spin(FORWARD, control_l1, PERCENT)
         claw_motor.spin(FORWARD, control_r1, PERCENT)
  
         # and the auxilary motors
-        motor_aux_1.spin(FORWARD, control_l2, PERCENT)
-        motor_aux_2.spin(FORWARD, control_r2, PERCENT)
+        # motor_aux_1.spin(FORWARD, control_l2, PERCENT)
+        # motor_aux_2.spin(FORWARD, control_r2, PERCENT)
 
         # No need to run too fast
         sleep(10)
